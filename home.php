93b1907780550f21280cce24c81cfa43a2be1348
sataw <satawat535@gmail.com>
2018-05-03 11:02:23 +0700

    arc2 sparql ontology query tool

--- home.php
+++ home.php
@@ -1,3 +1,17 @@
+<?php
+/* ARC2 static class inclusion */
+include_once('vendor/semsol/arc2/ARC2.php');
+
+$dbpconfig = array(
+    "remote_store_endpoint" => "http://localhost:3030/diet_project/query",
+);
+
+$store = ARC2::getRemoteStore($dbpconfig);
+
+if ($errs = $store->getErrors()) {
+    echo "<h1>getRemoteStore error<h1>";
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,14 +47,16 @@
                             <div class="card-body">
                                 <div class="partOne">
                                     <div class="row">
-                                        <div class="form-group col-sm-6">
+                                        <div class="form-group col-sm-8 offset-sm-2">
                                             <label for="gender">เพศ</label>
                                             <select class="form-control" name="gender" id="gender">
                                                 <option value="male">ชาย</option>
                                                 <option value="female">หญิง</option>
                                             </select>
                                         </div>
-                                        <div class="form-group col-sm-6">
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-sm-8 offset-sm-2">
                                             <label for="age">อายุ</label>
                                             <select class="form-control" name="age" id="age">
                                                 <?php for ($i = 1; $i < 100; $i++) {
@@ -50,7 +66,7 @@
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="form-group col-sm-6">
+                                        <div class="form-group col-sm-8 offset-sm-2">
                                             <label for="weight">น้ำหนัก</label>
                                             <select class="form-control" name="weight" id="weight">
                                                 <?php for ($i = 30; $i < 150; $i++) {
@@ -58,13 +74,59 @@
                                                 } ?>
                                             </select>
                                         </div>
-                                        <div class="form-group col-sm-6">
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-sm-8 offset-sm-2">
                                             <label for="height">ส่วนสูง</label>
                                             <select class="form-control" name="height" id="height">
                                                 <?php for ($i = 50; $i < 220; $i++) {
                                                     echo "<option value='" . $i . "' " . ($i == 170 ? 'selected' : '') . ">" . $i . "</option>";
                                                 } ?>
                                             </select>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-sm-8 offset-sm-2">
+                                            <label for="disease">โรคประจำตัว (ถ้ามี)</label>
+                                            <select class="form-control" name="disease" id="disease">
+                                                <option value="" disabled selected>ไม่มีโรคประจำตัว</option>
+                                                <?php
+                                                $activity_factors = '
+                                            PREFIX ex: <http://www.semanticweb.org/diet_project#>
+
+                                              SELECT ?disease_name
+                                            WHERE {
+                                                ?disease ex:disease_name ?disease_name .
+                                            }';
+                                                foreach ($store->query($activity_factors, 'rows') as $activity_factor) {
+                                                    echo "<option value='" . $activity_factor['disease_name'] . "'>" . $activity_factor['disease_name'] . "</option>";
+                                                }
+                                                ?>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-sm-8 offset-sm-2">
+                                            <label for="activity_factor">ระดับกิจกรรมในชีวิตประจำวัน</label>
+                                            <select class="form-control" name="activity_factor" id="activity_factor">
+                                                <option value="" disabled selected>โปรดเลือกข้อมูล</option>
+                                                <?php
+                                                $activity_factors = '
+                                            PREFIX ex: <http://www.semanticweb.org/diet_project#>
+
+                                              SELECT ?individual ?activity_factor_name ?activity_factor_description ?activity_factor_value
+                                            WHERE {
+                                              ?individual ex:activity_factor_name ?activity_factor_name .
+                                              ?individual ex:activity_factor_description ?activity_factor_description .
+                                              ?individual ex:activity_factor_value ?activity_factor_value .
+                                            }';
+                                                foreach ($store->query($activity_factors, 'rows') as $activity_factor) {
+                                                    echo "<option value='" . $activity_factor['activity_factor_value'] . "' data-description='" . $activity_factor['activity_factor_description'] . "'>" . $activity_factor['activity_factor_name'] . "</option>";
+                                                }
+                                                ?>
+                                            </select>
+                                            <div class="invalid-feedback activityFactorFeedback" style="display: none">โปรดเลือกข้อมูลระดับกิจกรรมในชีวิตประจำวันด้วย</div>
+                                            <div class="jumbotron pt-4 pb-4" style="display: none"></div>
                                         </div>
                                     </div>
                                     <hr>
@@ -77,26 +139,7 @@
                                 </div>
                                 <div class="partTwo" style="display: none;">
                                     <h4>BMR ของคุณคือ <strong id="bmrValue"></strong></h4>
-                                    <div class="row mt-3">
-                                        <div class="form-group col-sm-6">
-                                            <label for="height">โรคประจำตัว (ถ้ามี)</label>
-                                            <select class="form-control" name="height" id="height">
-                                                <option value="" selected>ไม่มีโรคประจำตัว</option>
-                                                <option value="">โรคหัวใจ</option>
-                                                <option value="">โรคหอบหืด</option>
-                                                <option value="">โรคความดันโลหิตสูง</option>
-                                                <option value="">โรคอ้วน</option>
-                                                <option value="">โรคข้อเข่าเสื่อม</option>
-                                            </select>
-                                        </div>
-                                        <div class="form-group col-sm-6">
-                                            <label for="height">ระดับกิจกรรมในชีวิตประจำวัน</label>
-                                            <select class="form-control" name="height" id="height">
-                                                <option value="">ทำงานอยู่กับที่ เคลื่อนไหวน้อย</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="row">
+                                    <div class="row mt-5">
                                         <div class="form-group col-sm-6">
                                             <label for="weightGoal">น้ำหนักที่ต้องการลด</label>
                                             <select class="form-control" name="weightGoal" id="weightGoal">
@@ -139,21 +182,33 @@
         let age = $("select[name='age']").val();
         let weight = $("select[name='weight']").val();
         let height = $("select[name='height']").val();
+        let activity_factor = $("select[name='activity_factor']").val();
         let bmr = null;
 
-        if (gender === 'male') {
-            //สูตรของ Mifflin – St Jeor
-            bmr = (10 * weight) + (6.25 * height) - (5 * (age + 5));
-            $('#bmrValue').html(bmr);
-            $('.partOne').hide(200);
-            $('.partTwo').show(200);
+        if (activity_factor === '' || activity_factor === null) {
+            $('.activityFactorFeedback').show();
+            $('#activity_factor').addClass('is-invalid');
         } else {
-            //สูตรของ Mifflin – St Jeor
-            bmr = (10 * weight) + (6.25 * height) - (5 * (age - 161));
-            $('#bmrValue').html(bmr);
-            $('.partOne').hide(200);
-            $('.partTwo').show(200);
+            if (gender === 'male') {
+                //สูตรของ Mifflin – St Jeor
+                bmr = (10 * weight) + (6.25 * height) - (5 * (age + 5));
+                $('#bmrValue').html(bmr);
+                $('.partOne').hide(200);
+                $('.partTwo').show(200);
+            } else {
+                //สูตรของ Mifflin – St Jeor
+                bmr = (10 * weight) + (6.25 * height) - (5 * (age - 161));
+                $('#bmrValue').html(bmr);
+                $('.partOne').hide(200);
+                $('.partTwo').show(200);
+            }
         }
+    });
+
+    $(document).on('change', '#activity_factor', e => {
+        $('.jumbotron').html('<p class="lead">' + $(e.currentTarget).find(':selected').attr('data-description') + '</p>').show();
+        $('.activityFactorFeedback').hide();
+        $('#activity_factor').removeClass('is-invalid').addClass('is-valid');
     });
 </script>
 
--- index.php
+++ index.php
@@ -1,36 +1,6 @@
 <html>
 <body>
 
-<?php
-/* ARC2 static class inclusion */
-include_once('vendor/semsol/arc2/ARC2.php');
 
-$dbpconfig = array(
-    "remote_store_endpoint" => "http://localhost:3030/exercise_project/sparql",
-);
-
-$store = ARC2::getRemoteStore($dbpconfig);
-
-if ($errs = $store->getErrors()) {
-    echo "<h1>getRemoteSotre error<h1>";
-}
-
-$query = '
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-  SELECT ?subject ?predicate ?object
-WHERE {
-  ?subject ?predicate ?object
-}
-LIMIT 25';
-
-/* execute the query */
-$rows = $store->query($query, 'rows');
-print_r($rows);
-//if ($errs = $store->getErrors()) {
-//    echo "Query errors";
-//    print_r($errs);
-//}
-?>
 </body>
 </html>
